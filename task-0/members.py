2e66e854d2c80d808fb584ca427ef57bddc8eb93
Theerapat  Anan <kaowpun83@gmail.com>
2023-09-12 21:24:23 +0700

    Update members.py

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Theerapat Anan", "sid": "6422780179"},
+    {"name": "Vorapol Assawasongkram", "sid": "6422772101"},
 ]
